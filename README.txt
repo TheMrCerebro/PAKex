283e4d85c78cd20225c804a6f2113c27863bdfd7
TheMrCerebro <99989085+TheMrCerebro@users.noreply.github.com>
2022-04-05 11:55:21 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,54 @@
+ _______  _______  ___   _  _______  __   __ 
+|       ||   _   ||   | | ||       ||  |_|  |
+|    _  ||  |_|  ||   |_| ||    ___||       |
+|   |_| ||       ||      _||   |___ |       |
+|    ___||       ||     |_ |    ___| |     | 
+|   |    |   _   ||    _  ||   |___ |   _   |
+|___|    |__| |__||___| |_||_______||__| |__|
+                                                    
+FAST AND EASY PAK FILE EXTRACTOR
+
+A tool created by TheMrCerebro
+Copyright (C) 2022 TheMrCerebro
+
+
+=============
+Introduction:
+=============
+
+With this tool you can extract all files contained in any PAK file
+used by Half-life and Quake games.
+
+
+============
+Legal stuff:
+============
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+
+==================
+Program Changelog:
+==================
+
+  v1.0.0 Alpha 1
+  - Initial release.
+
+
+================
+Troubleshooting:
+================
+
+Is there a problem?
+Refer to my site for information and ways to contact me if there is a problem.
+I appreciate bug-reports and feedback.
+
+  email: themrcerebro@gmail.com
---
+++ main.cpp
@@ -0,0 +1,158 @@
+/*
+ *
+ * Copyright (c) 2022 TheMrCerebro
+ *
+ * PAKex - Zlib license.
+ *
+ * This software is provided 'as-is', without any express or
+ * implied warranty. In no event will the authors be held
+ * liable for any damages arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute
+ * it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented;
+ * you must not claim that you wrote the original software.
+ * If you use this software in a product, an acknowledgment
+ * in the product documentation would be appreciated but
+ * is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such,
+ * and must not be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any
+ * source distribution.
+ *
+*/
+#include <vector>
+#include <iostream>
+#include <fcntl.h>
+
+using namespace std;
+
+struct SPAKFileHeader
+{
+    char tag[4];
+    int offset;
+    int length;
+};
+
+struct SPAKFileEntry
+{
+    char name[56];
+    int offset;
+    int length;
+};
+
+int main()
+{
+    cout << "               _______  _______  ___   _  _______  __   __ " << endl;
+    cout << "              |       ||   _   ||   | | ||       ||  |_|  |" << endl;
+    cout << "              |    _  ||  |_|  ||   |_| ||    ___||       |" << endl;
+    cout << "              |   |_| ||       ||      _||   |___ |       |" << endl;
+    cout << "              |    ___||       ||     |_ |    ___| |     | " << endl;
+    cout << "              |   |    |   _   ||    _  ||   |___ |   _   |" << endl;
+    cout << "              |___|    |__| |__||___| |_||_______||__| |__|" << endl;
+    cout << " (o)=================================================================(o)" << endl;
+    cout << "          - FAST AND EASY PAK FILE EXTRACTOR [by TheMrCerebro] -" << endl;
+    cout << " (o)=================================================================(o)" << endl;
+    cout << endl;
+    cout << "     With this tool you can extract all files contained in any PAK" << endl;
+    cout << "              file used by Half-life and Quake games." << endl;
+    cout << endl;
+
+    cout << " File: ";
+
+    string path;
+    cin >> path;
+
+    cout << endl;
+
+    // Crea la carpeta principal con el nombre del archivo ".pak" donde se va a extraer el contenido.
+    std::string folder;
+    for(size_t i=0; i<path.find(".");++i)
+        folder += path[i];
+
+    mkdir(folder.c_str());
+    chdir(folder.c_str());
+
+    // Hubicacion de la carpeta principal.
+    char cwd[256];
+    getcwd(cwd, 256);
+
+	SPAKFileHeader header;
+
+	FILE *pf = fopen(path.c_str(),"rb");
+	fread(&header, 1, sizeof(header), pf);
+
+    char* tag = header.tag;
+	if(tag[0] == 'P' && tag[1] == 'A' && tag[2] == 'C' && tag[3] == 'K')
+    {
+        cout << " Valid PAK file" << endl;
+        cout << endl;
+
+        fseek(pf, header.offset, SEEK_SET);
+
+        const int numberOfFiles = header.length / sizeof(SPAKFileEntry);
+
+        vector<SPAKFileEntry> entryA;
+        for(int i=0; i<numberOfFiles; i++)
+        {
+            // read an entry
+            SPAKFileEntry entry;
+            fread(&entry, 1, sizeof(entry), pf);
+            entryA.push_back(entry);
+        }
+
+        for(int i=0; i<numberOfFiles; ++i)
+        {
+            string tmp;
+            for(size_t j=0; j<strlen(entryA[i].name); ++j)
+            {
+                if(entryA[i].name[j]=='/')
+                {
+                    if((int)tmp.find(".") > -1) break;
+                    mkdir(tmp.c_str());
+                    chdir(tmp.c_str());
+                    tmp.clear();
+                }
+                else
+                {
+                    tmp += entryA[i].name[j];
+                }
+            }
+
+            chdir(cwd);
+
+            char *buffer = new char[entryA[i].length];
+
+            fseek(pf, entryA[i].offset, SEEK_SET);
+            fread(buffer, 1, entryA[i].length, pf);
+
+            FILE *wf = fopen(entryA[i].name,"wb");
+            fwrite(buffer, 1, entryA[i].length, wf);
+            fclose(wf);
+
+            delete [] buffer;
+
+            cout << "  " << entryA[i].name << endl;
+        }
+
+        cout << endl;
+        cout << " Total Files: " << numberOfFiles << endl;
+        cout << endl;
+        cout << " ...Finished" << endl;
+        cout << endl;
+    }
+    else
+    {
+        cout << " Invalid PAK file" << endl;
+        cout << endl;
+    }
+
+    fclose(pf);
+
+    system("pause");
+    return 0;
+}
